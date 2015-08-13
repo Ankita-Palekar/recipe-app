251bc738dcf1e097b9a408ffd1ac6c7de2338c7c
Ankita-Palekar <453ankitapalekar@gmail.com>
2015-08-13 11:21:51 +0530

    removed unwated javascript files

--- config/deploy.rb
+++ config/deploy.rb
@@ -9,6 +9,7 @@ set :repo_url, 'git@bitbucket.org:vacationlabs/ankita-recipe-project.git'
 
 # Default branch is :master
 # ask :branch, `git rev-parse --abbrev-ref HEAD`.chomp
+set :branch, fetch(:branch, "master")
 
 # Default deploy_to directory is /var/www/my_app_name
 set :deploy_to, '/var/www/html/recipe-app'
--- config/environment.rb
+++ config/environment.rb
@@ -2,4 +2,4 @@
 require File.expand_path('../application', __FILE__)
 # Initialize the rails application
 Foodholic::Application.initialize!
-ENV['RAILS_ENV'] ||= 'production'
+
