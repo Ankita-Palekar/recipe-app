afff6669a2203d9a39d1e215a7a9c728e5b00de2
Ankita-Palekar <453ankitapalekar@gmail.com>
2015-08-19 07:51:20 +0530

    cors

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,8 +2,15 @@ class ApplicationController < ActionController::Base
   # protect_from_forgery
   # @@TODO remember to add message when trying out on actions not authorised to him 
   include SessionsHelper
+  before_filter :allow_cross_domain_access
+  
   rescue_from CanCan::AccessDenied do | exception |
     redirect_to root_url, notice: exception.message
+  end
+
+  def allow_cross_domain_access
+    response.headers["Access-Control-Allow-Origin"] = "*"
+    response.headers["Access-Control-Allow-Methods"] = "*"
   end
 
 end
--- config/application.rb
+++ config/application.rb
@@ -58,10 +58,5 @@ module Foodholic
 
     # Version of your assets, change this if you want to expire all your assets
     config.assets.version = '1.0'
-
-    config.action_dispatch.default_headers.merge!({
-      'Access-Control-Allow-Origin' => 'http://1925030143.rsc.cdn77.org',
-      'Access-Control-Request-Method' => '*'
-    })
   end
 end
