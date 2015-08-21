aa50e955e3a5d5a9b9e17db655d05a268d9b364d
Ankita-Palekar <453ankitapalekar@gmail.com>
2015-08-21 10:37:20 +0530

    cors header added

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,8 +2,17 @@ class ApplicationController < ActionController::Base
   # protect_from_forgery
   # @@TODO remember to add message when trying out on actions not authorised to him 
   include SessionsHelper
+  
   rescue_from CanCan::AccessDenied do | exception |
     redirect_to root_url, notice: exception.message
   end
+
+  before_filter :allow_cross_domain_access
+  def allow_cross_domain_access
+    response.headers["Access-Control-Allow-Origin"] = "*"
+    response.headers["Access-Control-Allow-Methods"] = "*"
+  end
+
+
 
 end
