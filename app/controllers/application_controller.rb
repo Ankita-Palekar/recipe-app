ff2b6df7b751a46de45fb7e63e344a142164fdb0
Ankita-Palekar <453ankitapalekar@gmail.com>
2015-08-21 20:55:37 +0530

    cors

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -9,8 +9,8 @@ class ApplicationController < ActionController::Base
 
   before_filter :allow_cross_domain_access
   def allow_cross_domain_access
-    response.headers["Access-Control-Allow-Origin"] = "*"
-    response.headers["Access-Control-Allow-Methods"] = "*"
+    request.headers["Access-Control-Allow-Origin"] = "*"
+    request.headers["Access-Control-Allow-Methods"] = "*"
   end
 
 
