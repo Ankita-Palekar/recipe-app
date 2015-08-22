e1e763adf4e6e94786cb147080d6e8dea3fbb4e5
Pradosh Gaonkar <pradosh@vacationlabs.com>
2015-08-22 04:21:56 +0000

    CORS header added

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -13,6 +13,16 @@ class ApplicationController < ActionController::Base
     request.headers["Access-Control-Allow-Methods"] = "*"
   end
 
+  
+  def set_headers
+    headers['Access-Control-Allow-Origin'] = '*'
+    headers['Access-Control-Expose-Headers'] = 'ETag'
+    headers['Access-Control-Allow-Methods'] = 'GET, POST, PATCH, PUT, DELETE, OPTIONS, HEAD'
+    headers['Access-Control-Allow-Headers'] = '*,x-requested-with,Content-Type,If-Modified-Since,If-None-Match'
+    headers['Access-Control-Max-Age'] = '86400'
+  end
+
+
 
 
 end
